d7f5482bb67f24f63257cbeb0b1dccd143657860
Mark Dodson <mark.dodson@slatergordon.co.uk>
2016-12-08 13:43:49 +0000

    Tutorial 3.2 & 3.3

--- Unity/FirstTestingProject/Assets/Scripts/PlayerController.cs
+++ Unity/FirstTestingProject/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
+
 	private Rigidbody rigidBody;
+	private int count;
 
 	void Start(){
-		
+		count = 0;
+		SetCountText ();
+		winText.gameObject.SetActive (false);
 		rigidBody = GetComponent<Rigidbody> ();		
 	}
 
@@ -17,6 +24,28 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
 		rigidBody.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject);
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			count++;
+			SetCountText ();
+			other.gameObject.SetActive (false);
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 10) 
+		{
+			winText.gameObject.SetActive (true);
+		}
+		//if (count >= 10) {
+		//	winText.text = "You Win!";
+		//}
 	}
 			
 }
